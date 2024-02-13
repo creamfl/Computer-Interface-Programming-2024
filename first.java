efe23dd47b08b896fde9ec7284bcb30a8fbd7794
Niki Ramanchev <147237222+creamfl@users.noreply.github.com>
2024-02-13 19:01:08 +0100

    first.java

---
+++ first.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Instant;
+
+public class Main {
+    public static void main(String[] args) {
+        String fileName = "uniqueFile_" + Instant.now().getEpochSecond() + ".txt";
+        File file = new File(fileName);
+
+        if (file.exists()) {
+            System.out.println("File already exists");
+        } else {
+            try {
+                if (file.createNewFile()) {
+                    System.out.println("File created: " + file.getName());
+                    System.out.println("Absolute path: " + file.getAbsolutePath());
+
+                    Path pathAbsolute = Paths.get(file.getAbsolutePath());
+                    Path pathBase = Paths.get(file.getAbsolutePath()).getParent();
+                    Path pathRelative = pathBase.relativize(pathAbsolute);
+                    System.out.println("Relative path: " + pathRelative);
+
+                    System.out.println("Write privileges: " + file.canWrite());
+                    System.out.println("Read privileges: " + file.canRead());
+
+                    System.out.println("Parent folder: " + file.getParent());
+                } else {
+                    System.out.println("File creation failed");
+                }
+            } catch (IOException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
+    }
+}
