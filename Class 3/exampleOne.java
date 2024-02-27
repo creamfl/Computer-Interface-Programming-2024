6570aae58aa6db1f5d5d191c2dddf755aa1fff35
niki <niki.ramanchev@gmail.com>
2024-02-27 19:21:02 +0100

    commit exampleOne.java

---
+++ Class 3/exampleOne.java
@@ -0,0 +1,25 @@
+ThreadGroup rootGroup = Thread.currentThread().getThreadGroup();
+        ThreadGroup parentGroup;
+        while ((parentGroup = rootGroup.getParent()) != null) {
+        rootGroup = parentGroup;
+        }
+
+        Thread[] threads = new Thread[rootGroup.activeCount()];
+        while (rootGroup.enumerate(threads, true) == threads.length) {
+        threads = new Thread[threads.length * 2];
+        }
+
+        for (int i = 0; i < threads.length; i++) {
+        Thread thread = threads[i];
+        if (thread != null) {
+        System.out.println("Name: " + thread.getName());
+        System.out.println("Priority: " + thread.getPriority());
+        System.out.println("Is Alive: " + thread.isAlive());
+        System.out.println("Thread Declaration: Name - " + thread.getName() + ", Priority - " + thread.getPriority() + ", Group - " + thread.getThreadGroup().getName());
+
+        if (thread.getPriority() < Thread.MAX_PRIORITY) {
+        thread.setPriority(thread.getPriority() + 1);
+        System.out.println("New Priority: " + thread.getPriority());
+        }
+        }
+        }
